cec685332e6390c3fb44f1a3a2b783f218ef1bc7
mlendale <ml.endale@gmail.com>
2016-10-11 15:15:25 +0200

    Fixed session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::SessionsController < Api::V1::BaseController
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: create_params[:email])
+    if user && user.authenticate(create_params[:password])
       self.current_user = user
       render jsonapi: user, status: :ok, serializer: Api::V1::SessionSerializer
     else
-      render jsonapi: "User not authorized #{params[:email].nil?.to_s}", status: 401
+      render jsonapi: "User not authorized #{create_params[:email].nil?.to_s}", status: 401
     end
   end
 
+  private
+  def create_params
+	ActiveModelSerializers::Deserialization.jsonapi_parse!(params, only: [:email, :password] )
+#    params.permit(:email, :password)
+  end
 end
